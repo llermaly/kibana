5dec75bb22f5bf392f4df3135965797257e07006
Tre' Seymour <wayne.seymour@elastic.co>
2022-03-08 16:13:41 +0000

    Change path to begin at root of prj.

--- .buildkite/scripts/steps/code_coverage/data_retention/truncate_data.sh
+++ .buildkite/scripts/steps/code_coverage/data_retention/truncate_data.sh
@@ -1,7 +1,9 @@
-filename="./2020_dirs_file.txt"
+filename=".buildkite/scripts/steps/code_coverage/data_retention/2020_dirs_file.txt"
+#filename=".buildkite/scripts/steps/code_coverage/data_retention/test_dirs_file.txt"
 
 while read line
 do
   echo "### Truncating $line"
   gsutil -m rm -r gs://elastic-bekitzur-kibana-coverage-live/$line
+#  gsutil ls gs://elastic-bekitzur-kibana-coverage-live/$line
 done < $filename
